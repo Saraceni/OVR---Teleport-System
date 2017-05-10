fd8b72c69ada8bd1effb8896a9ebb61ea95b1f59
Saraceni <saraceni.br@gmail.com>
2017-05-09 23:00:04 -0300

    First Commit

---
+++ Assets/Scripts/Teleporter.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour {
+
+	public GameObject TeleportMarker;
+	public Transform Player;
+	public float RayLength = 50f;
+
+	// Use this for initialization
+	void Start () {
+		OVRTouchpad.Create ();
+		OVRTouchpad.TouchHandler += TouchpadHandler;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Ray ray = new Ray (transform.position, transform.forward);
+		RaycastHit hit;
+		if (Physics.Raycast (ray, out hit, RayLength)) {
+			if (hit.collider.tag == "Ground") {
+				if (!TeleportMarker.activeSelf) {
+					TeleportMarker.SetActive (true);
+				}
+				TeleportMarker.transform.position = hit.point;
+			} else {
+				TeleportMarker.SetActive (false);
+			}
+		} else {
+			TeleportMarker.SetActive (false);
+		}
+	}
+
+	void TouchpadHandler(object sender, System.EventArgs e) {
+
+		OVRTouchpad.TouchArgs args = (OVRTouchpad.TouchArgs)e;
+		if (args.TouchType == OVRTouchpad.TouchEvent.SingleTap) {
+			// Need to check where the user is looking to teleport him
+			if (TeleportMarker.activeSelf) {
+				Vector3 markerPosition = TeleportMarker.transform.position;
+				Player.position = new Vector3 (markerPosition.x, Player.position.y, 
+					markerPosition.z);
+			}
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,24 @@
+## Synopsis
+
+This is a teleport system for Oculus Based VR Headsets ( Oculus Rift or Gear VR ) made with Unity. If you look to the floor a mark will appear and 
+when you tap a touch control you will be teleported to the mark.
+
+At the top of the file there should be a short introduction and/ or overview that explains **what** the project is. This description should match descriptions added for package managers (Gemspec, package.json, etc.)
+
+## Motivation
+
+The teleport system is one of the best ways to move in a VR environment avoiding motion sickness. 
+
+## Contributors
+
+Rafael Saraceni
+
+Twitter: https://twitter.com/saraceni_br
+Linkedin: https://www.linkedin.com/in/rafaelsaraceni/
+Facebook: https://www.facebook.com/klaus.leafar
+
+## License
+
+The MIT License (MIT)
+
+Copyright (c) 2017 Rafael Saraceni
